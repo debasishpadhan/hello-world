df2edda46500b8a852b81ef2c5309d1e3c9fa8fc
Debasish Padhan <53099388+debasishpadhan@users.noreply.github.com>
2022-01-19 18:06:26 +0530

    Update ecs-cluster-task.sh

--- ecs-cluster-task.sh
+++ ecs-cluster-task.sh
@@ -1,8 +1,9 @@
 aws ecs create-cluster --cluster-name fargate-cluster
-aws ecs register-task-definition --cli-input-json file://$HOME/task-definition.json
-aws ecs list-task-definitions |grep hello_user
-aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition hello_user:4 --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0bba38ef988e68656,subnet-0d5fb5b19d05062b9],securityGroups=[sg-00deb8fbfc3520975],assignPublicIp=ENABLED}"
+aws ecs register-task-definition --cli-input-json file://task-definition.json
+aws ecs list-task-definitions | grep hello_user
+REVISION=`aws ecs describe-task-definition --task-definition hello_user | jq .taskDefinition.revision`
+aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition hello_user:$REVISION --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0bba38ef988e68656,subnet-0d5fb5b19d05062b9],securityGroups=[sg-00deb8fbfc3520975],assignPublicIp=ENABLED}"
 aws ecs list-services --cluster fargate-cluster
-sleep 10m
+sleep 30m
 aws ecs delete-service --cluster fargate-cluster --service fargate-service --force
 aws ecs delete-cluster --cluster fargate-cluster
