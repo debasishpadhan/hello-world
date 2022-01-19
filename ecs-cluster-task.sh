a6a59e6a0d9e722f4bf228a379459a8e2103878b
Debasish Padhan <53099388+debasishpadhan@users.noreply.github.com>
2022-01-19 20:00:22 +0530

    Update ecs-cluster-task.sh

--- ecs-cluster-task.sh
+++ ecs-cluster-task.sh
@@ -1,10 +1,12 @@
 #aws ecs create-cluster --cluster-name fargate-cluster
-aws ecs register-task-definition --cli-input-json file://task-definition.json
+sed "s/version*/version${env.BUILD_NUMBER}/g" task-definition.json
+cat task-definition.json
+#aws ecs register-task-definition --cli-input-json file://task-definition.json
 
 #aws ecs list-task-definitions | grep hello-docker
-REVISION=`aws ecs describe-task-definition --task-definition hello-docker | jq .taskDefinition.revision`
+#REVISION=`aws ecs describe-task-definition --task-definition hello-docker | jq .taskDefinition.revision`
 
-aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition hello-docker:$REVISION --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0bba38ef988e68656,subnet-0d5fb5b19d05062b9],securityGroups=[sg-00deb8fbfc3520975],assignPublicIp=ENABLED}"
+#aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition hello-docker:$REVISION --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0bba38ef988e68656,subnet-0d5fb5b19d05062b9],securityGroups=[sg-00deb8fbfc3520975],assignPublicIp=ENABLED}"
 #aws ecs list-services --cluster fargate-cluster
 
 #aws ecs delete-service --cluster fargate-cluster --service fargate-service --force
